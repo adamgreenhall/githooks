377bfdc5c23bca8560166c01274b357b787fb87e
Adam Greenhall <agreenhall@lyft.com>
2013-10-09 14:55:12 -0700

    check for ipdb and from pdb import as well

--- pre-commit.pdb.py
+++ pre-commit.pdb.py
@@ -7,15 +7,15 @@ from git_utils import (
     get_filenames_from_git_stage, get_file_from_git_stage)
 
 
-pdb_re = re.compile("import\s+pdb")
+pdb_import_re = re.compile("import\s+i?pdb")
+pdb_from_re = re.compile("from\s+i?pdb")
 exit_status = 0
-
 for filename in get_filenames_from_git_stage(".py"):
     content = get_file_from_git_stage(filename)
     line_no = 0
     for line in content.split("\n"):
         line_no += 1
-        if pdb_re.search(line):
+        if pdb_import_re.search(line) or pdb_from_re.search(line):
             print "%s %d: !!! DEBUG CODE !!! %s" % (filename, line_no, line)
             exit_status = 1
 
