eaff239363b10700c3ad37ff8178abb797f5ab6d
Adam Greenhall <agreenhall@lyft.com>
2013-10-09 18:08:41 -0700

    use pyflakes, but don't use pep8 (for now)

--- pre-commit.pdb.py
+++ pre-commit.pdb.py
@@ -2,7 +2,7 @@
 
 import re
 import sys
-
+from pyflakes.api import checkPath
 from git_utils import (
     get_filenames_from_git_stage, get_file_from_git_stage)
 
@@ -11,12 +11,15 @@ pdb_import_re = re.compile("import\s+i?pdb")
 pdb_from_re = re.compile("from\s+i?pdb")
 exit_status = 0
 for filename in get_filenames_from_git_stage(".py"):
+    if checkPath(filename) > 0:
+      exit_status = 1
     content = get_file_from_git_stage(filename)
     line_no = 0
     for line in content.split("\n"):
         line_no += 1
         if pdb_import_re.search(line) or pdb_from_re.search(line):
-            print "%s %d: !!! DEBUG CODE !!! %s" % (filename, line_no, line)
+            print "%s %d: %s" % (filename, line_no, line)
             exit_status = 1
-
+if exit_status == 1:
+  print('commit failed')
 sys.exit(exit_status)
