b7858da0307725e30e87145428648f601f4f1eb4
Adam Greenhall <agreenhall@lyft.com>
2013-10-16 09:42:03 -0700

    add IPython embed as debugger

--- pre-commit.pdb.py
+++ pre-commit.pdb.py
@@ -6,9 +6,10 @@ from pyflakes.api import checkPath
 from git_utils import (
     get_filenames_from_git_stage, get_file_from_git_stage)
 
+breakpoints_re = map(lambda pat: re.compile(pat), 
+    ["import\s+i?pdb", "from\s+i?pdb", "from\s+IPython", "import\s+IPython"]
+    )
 
-pdb_import_re = re.compile("import\s+i?pdb")
-pdb_from_re = re.compile("from\s+i?pdb")
 exit_status = 0
 for filename in get_filenames_from_git_stage(".py"):
     if checkPath(filename) > 0:
@@ -17,8 +18,10 @@ for filename in get_filenames_from_git_stage(".py"):
     line_no = 0
     for line in content.split("\n"):
         line_no += 1
-        if pdb_import_re.search(line) or pdb_from_re.search(line):
-            print "%s %d: %s" % (filename, line_no, line)
+        if reduce(lambda a, b: a or b, 
+            map(lambda pat: pat.search(line), breakpoints_re)):
+            # pdb_import_re.search(line) or pdb_from_re.search(line)
+            print "%s has bp in ln#%d: %s" % (filename, line_no, line)
             exit_status = 1
 if exit_status == 1:
   print('commit failed')
